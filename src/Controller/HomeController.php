aef112089975bf6cb5161d187592f539627e4341
Espero AKPOLI <contact@espero-soft.com>
2021-01-17 17:52:54 +0100

    Product, Categories & Admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,24 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Entity\RelatedProducts;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(ProductRepository $repo, EntityManagerInterface $manager): Response
+    public function index(ProductRepository $repoProduct): Response
     {
-      //  dd($repo->findAll());
+        $products = $repoProduct->findAll();
 
+        $productBestSeller = $repoProduct->findByIsBestSeller(1);
+
+        $productSpecialOffer = $repoProduct->findByIsSpecialOffer(1);
+
+        $productNewArrival = $repoProduct->findByIsNewArrival(1);
+
+        $productFeatured = $repoProduct->findByIsFeatured(1);
+
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products' => $products,
+            'productBestSeller' => $productBestSeller,
+            'productSpecialOffer' => $productSpecialOffer,
+            'productNewArrival' => $productNewArrival,
+            'productFeatured' => $productFeatured
         ]);
     }
+
+    /**
+     * @Route("/product/{slug}", name="product_details")
+     */
+    public function show(?Product $product): Response{
+        
+        if(!$product){
+            return $this->redirectToRoute("home");
+        }
+
+        return $this->render("home/single_product.html.twig",[
+            'product' => $product
+        ]);
+    }
+
+   
 }
